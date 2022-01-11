3de3d4449b4bc5a585510b8f111c2108a86d90e4
willbillionquant <willbillion2017@gmail.com>
2022-01-11 16:46:49 +0800

    Update update_ny.py

--- update_ny.py
+++ update_ny.py
@@ -1,30 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 16 20:26:43 2020
-
-@author: willbillion
-"""
-
 import os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
 import sys
 sys.path.append('..')
 
 from litedata import basepath
-
 import logging
 
 from litedata import Litedata
-from init_db import init_dbsetup
+from setupdb import init_dbsetup
 dbfile = os.path.join(basepath, 'global_data.db')
 if not os.path.exists(dbfile):
     init_dbsetup()
 
 from datetime import datetime
 currenttime = datetime.now().strftime('%Y%m%d')
-logfile = 'liteupdate_%s.txt'%(currenttime)
-logging.basicConfig(filename=logfile, level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logfile = f'liteupdate_{currenttime}.txt'
+logging.basicConfig(filename=logfile, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.info('START Updating US data:')
 
 from updatesectype_flow import update_sectypedata
