1c12fd195d982250e8cff09d40ed7663072ba02d
willbillionquant <willbillion2017@gmail.com>
2022-01-11 16:14:33 +0800

    Update __init__.py

---
+++ __init__.py
@@ -0,0 +1,15 @@
+import os
+codepath = os.path.dirname(os.path.abspath(__file__))
+import sys
+sys.path.append('..')
+
+from configparser import ConfigParser
+configdata = ConfigParser()
+configdata.read(os.path.join(codepath, 'setting_litedata.ini'))
+
+# File path
+basepath = configdata['paths'].get('basepath')
+logpath = os.path.join(basepath, 'logs')
+for path in [basepath, logpath]:
+    if not os.path.exists(path):
+        os.makedirs(path)
