e0776d65025cca9005d1490315151965fc58a360
willbillionquant <willbillion2017@gmail.com>
2022-06-26 17:10:59 +0800

    Update updatesectype_flow.py

--- updatesectype_flow.py
+++ updatesectype_flow.py
@@ -16,8 +16,8 @@ def update_sectypedata(sectype, symbollist_test, vecdelta=(16, 0), tzoffset=6, e
     allsymboldict = Yahoodata.get_symboldict_db()
     symboldict = {symbol: spec for symbol, spec in allsymboldict.items() if spec[1] == sectype}
     Yahoodata.updatedf_symboldict(symboldict, symbollist_test, vecdelta)
-    #export_date = datetime.now() - timedelta(hours=tzoffset)
-    #if export_date.weekday() >= export_wkday - 1:
-    #    Yahoodata.export_csv_symbollist(sectype)
+    export_date = datetime.now() - timedelta(hours=tzoffset)
+    if export_date.weekday() >= export_wkday - 1:
+        Yahoodata.export_csv_symbollist(sectype)
         
 logging.shutdown()
